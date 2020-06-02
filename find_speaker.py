63b6c5471a82cf6ac8d920b65054d4577b16cd83
ivankp <ivan.pogrebnyak@gmail.com>
2020-06-02 00:57:21 -0400

    2to3 find_speaker.py

--- find_speaker.py
+++ find_speaker.py
@@ -1,25 +1,25 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
 if len(sys.argv)!=3:
-    print "usage:", sys.argv[0], "file speaker"
+    print("usage:", sys.argv[0], "file speaker")
     sys.exit(1)
 
 import sys, yaml, re
 
 with open(sys.argv[1],'r') as f:
-    meetings = yaml.load(f)
+    meetings = yaml.load(f, Loader=yaml.SafeLoader)
 
 for m in meetings:
     if len(m)>2:
         for t in m[2]['talks']:
             for s in t['speakers']:
                 if sys.argv[2] in s[0]:
-                    print re.sub('T.*','',m[2]['time'])
-                    print t['title']
+                    print(re.sub('T.*','',m[2]['time']))
+                    print(t['title'])
                     for x in t['material']:
-                        print x
-                    print ''
+                        print(x)
+                    print()
                     break
 
